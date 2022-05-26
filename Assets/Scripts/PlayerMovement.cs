e533daefed2648d2c8198debc4c913237b3733bb
FacuSalco <facusalco26@gmail.com>
2022-05-26 20:37:41 -0300

    26/05/2022 Uptdate
    Hice agujas giratorias, relojitos que agregan tiempo y timer

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float movementSpeed = 0.1f, rotationSpeed = 4f;
+    //public RigidBody rb;
+
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 0, movementSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, 0, -movementSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, rotationSpeed, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, -rotationSpeed, 0);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(0, 0.3f, 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.Translate(0, 0, 0.025f);
+        }
+
+        //if (Input.GetKey(KeyCode.Space))
+        //{
+        //    rb.AddForce(new Vector3(0, 8f, 0), ForceMode.Impulse);
+        //}
+
+
+    }
+}
